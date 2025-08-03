ea35171a3b3fd16340d0ce01c6388cc94a6a1223
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-03 18:25:06 +0530

    themepicker: Init repo

---
+++ products/themepicker.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(WITH_GMS),true)
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/pixel/themepicker/common/common-vendor.mk)
+
+# Overlay
+PRODUCT_PACKAGES += \
+	PixelLauncherOverlayThemePicker
+
+PRODUCT_COPY_FILES += \
+    vendor/pixel/themepicker/prebuilt/etc/hiddenapi-package-whitelist-themepicker.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-hiddenapi-package-whitelist.xml \
+    vendor/pixel/themepicker/prebuilt/etc/preinstalled-packages-product-themepicker.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-preinstalled-packages-product-themepicker.xml \
+    vendor/pixel/themepicker/prebuilt/etc/sysconfig-themepicker.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/sysconfig-themepicker.xml \
+    vendor/pixel/themepicker/prebuilt/etc/privapp-permissions-themepicker-se.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/google-privapp-permissions-themepicker-se.xml
+
+endif
